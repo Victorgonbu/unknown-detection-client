3a74bf57646d1faae62fa936e591e1b95b865e76
Shaqri <peter200970@hotmail.com>
2021-08-26 00:46:17 -0500

    Add box shadow to App container and min-height when dropdown is active

--- src/components/presentationals/AuthLinks.js
+++ src/components/presentationals/AuthLinks.js
@@ -1,15 +1,15 @@
 import { Link } from 'react-router-dom';
-import { title, authLink } from '../../style/Dropdown.module.css';
+import { title, authLink, authLinks } from '../../style/Dropdown.module.css';
 
 function AuthLinks(props) {
    const { handleLink } = props;
 
   return(
-    <>
+    <div className={authLinks}>
       <h1 className={title}>Welcome to Unknown Detections! </h1>
       <Link onClick={handleLink} className={authLink} to="signup">Sign up</Link>
       <Link onClick={handleLink} className={authLink} to="login" >Log in</Link>
-    </>
+    </div>
   );
 };
 
