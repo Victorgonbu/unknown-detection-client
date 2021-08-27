1e96a1722bdf170ca577d9589cb7010aad99ad25
Shaqri <peter200970@hotmail.com>
2021-08-27 04:03:08 -0500

    Add search url to API file

--- src/API.js
+++ src/API.js
@@ -2,10 +2,12 @@ const SIGNUP = '/api/v1/users';
 const LOGIN = '/api/v1/auth';
 const POSTS = '/api/v1/posts';
 const FAVORITE_POSTS = '/api/v1/favorites';
+const SEARCH = '/api/v1/posts?search=' 
 
 export {
   SIGNUP,
   LOGIN,
   POSTS,
+  SEARCH,
   FAVORITE_POSTS,
 }
