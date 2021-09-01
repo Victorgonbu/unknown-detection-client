ee76d7e9e68b884236b994dd8106f605e27d1b12
Shaqri <peter200970@hotmail.com>
2021-09-01 15:26:45 -0500

    Add test cases for Dropdown button

---
+++ src/__tests__/presentationals/buttons/Dropdown.test.js
@@ -0,0 +1,32 @@
+import React from 'react';
+import {
+  render, fireEvent, screen
+} from '../../../utils/test-utils.js';
+import '@testing-library/jest-dom/extend-expect';
+import DropdownButton from '../../../components/presentationals/buttons/Dropdown';
+
+jest.mock('@fortawesome/react-fontawesome', () => ({
+  ...jest.requireActual('@fortawesome/react-fontawesome'),
+  FontAwesomeIcon: () => <div data-testid="icon"/>,
+}));
+
+const mockHandleClick = jest.fn();
+
+describe('DropdownButton', () => {
+  beforeEach(() => {
+    render(<DropdownButton handleClick={mockHandleClick}/>);
+  });
+  it('render button with bars icon', () => {
+    const button = screen.getByTestId('dropdown-button');
+    const icon = screen.getByTestId('icon');
+    expect(button).toBeInTheDocument();
+    expect(icon).toBeInTheDocument();
+  });
+
+  it('call function passed in props when clicked', () => {
+    const button = screen.getByTestId('dropdown-button');
+    fireEvent.click(button);
+    expect(mockHandleClick).toHaveBeenCalledTimes(1);
+    
+  });
+});
