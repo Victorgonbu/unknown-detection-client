a458dcee8f352bcc1f5aae8d75b6c75d76715066
Shaqri <peter200970@hotmail.com>
2021-09-01 19:15:22 -0500

    Add test cases for FooterLinks presentational

---
+++ src/__tests__/presentationals/FooterLinks.test.js
@@ -0,0 +1,25 @@
+import React from 'react';
+import {
+  render, screen, fireEvent
+} from '../../utils/test-utils';
+import '@testing-library/jest-dom/extend-expect';
+import FooterLinks from '../../components/presentationals/FooterLinks';
+
+const mockLogOut = jest.fn();
+
+describe('FooterLinks', () => {
+  it('render help label and log out button', () => {
+    render(<FooterLinks handleLogOut={() => {}}/>);
+    expect(screen.getByText('Help')).toBeInTheDocument();
+    expect(screen.getByText('Log out')).toBeInTheDocument();
+  });
+
+  it('call function passed in props when clicked', () => {
+    
+    render(<FooterLinks handleLogOut={mockLogOut} />);
+    const button = screen.getByText('Log out');
+    fireEvent.click(button);
+    expect(mockLogOut).toHaveBeenCalledTimes(1);
+
+  });
+});
